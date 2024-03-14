9f180930ba538f4f9aa13b77c0a802ae6e1bc228
ahodot01 <s.selection@gmail.com>
2024-03-14 15:25:01 +0000

    adding_comments_about_output

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
 # REQUESTING USER TO MAKE AN INPUT
 user_input = input("Please input something:")
 # PRINTING OUT VARIABLE THAT REPRESENT USER INPUT REQUESTED EARLIER
+# ADDING FORMATTING AND SOME TEXT TO OUTPUT
 print(f"Text, that user typed in earlier is {user_input}")
